a2b5545b30d200a3ab940249b9b24ebf182fb824
Doaa Hashim <doaa009@hotmail.com>
2019-10-29 12:21:49 +0200

    frist commit

--- BlogCore3/Program.cs
+++ BlogCore3/Program.cs
@@ -14,6 +14,7 @@ namespace BlogCore3
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+                //jjj
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
